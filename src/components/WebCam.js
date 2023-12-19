d71b9fa2663b24ecf709b68d41035046295fc5ca
abhayChandra01 <a4abhaychandra@gmail.com>
2023-12-19 10:57:09 +0530

    changes

--- src/components/WebCam.js
+++ src/components/WebCam.js
@@ -1,15 +1,18 @@
 import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
-const videoConstraints = {
-  width: 640,
-  height: 480,
+const backCamConstraints = {
   facingMode: { exact: "environment" },
+};
+
+const frontCamConstraints = {
+  facingMode: "user",
 };
 
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+  const [camToggle, setCamToggle] = useState(false);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -24,10 +27,11 @@ const WebcamCapture = () => {
         audio={false}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
+        videoConstraints={camToggle ? frontCamConstraints : backCamConstraints}
         minScreenshotWidth={180}
         minScreenshotHeight={180}
       />
+      <button onClick={() => setCamToggle(!camToggle)}>SPIN CAM</button>
       <button onClick={capture}>Capture Photo</button>
       {imgSrc && <img src={imgSrc} alt="img" width={100} height={100} />}
     </div>
